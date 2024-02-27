0b8d17fec2f35b270b9b07e63be0c4cbea8af393
Tsarkov Kirill <Tsarkovkirill@gmail.com>
2024-02-27 12:29:25 +0300

    Add envibox button

--- script.js
+++ script.js
@@ -16,4 +16,21 @@ companyButton.addEventListener('click', openCompanyList);
 connectButton.addEventListener('click', openConnectList);
 
 
+window.chat24_token = "f0f61b9b584238446bd86b16fd6ca1e6";
+window.chat24_url = "https://livechatv2.chat2desk.com";
+window.chat24_socket_url = "wss://livechatv2.chat2desk.com/widget_ws_new";
+window.chat24_show_new_wysiwyg = "true";
+window.chat24_static_files_domain = "https://storage.chat2desk.com/";
+window.lang = "ru";
+window.fetch("".concat(window.chat24_url, "/packs/manifest.json?nocache=").concat(new Date().getTime())).then(function (res) {
+    return res.json();
+}).then(function (data) {
+    var chat24 = document.createElement("script");
+    chat24.type = "text/javascript";
+    chat24.async = true;
+    chat24.src = "".concat(window.chat24_url).concat(data["application.js"]);
+    document.body.appendChild(chat24);
+});
+
+
 
